e1dc0e4146af97f19b4296a1c3e84552681e1901
yujen77300 <yujenhuang24@gmail.com>
2024-10-22 08:30:14 +0800

    test(hw2): add benchmark test

--- HW2/problem1/dec_to_bin_test.go
+++ HW2/problem1/dec_to_bin_test.go
@@ -1,6 +1,7 @@
 package problem1
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,5 +36,49 @@ func TestDecToBinRec(t *testing.T) {
 	for _, tc := range testCases {
 		binary := decToBinRec(tc.input)
 		assert.Equal(t, tc.expected, binary, "For input %d", tc.input)
+	}
+}
+
+func BenchmarkDecToBinIte(b *testing.B) {
+	testCases := []int{0, 1, 10, 100, 1000, 10000}
+
+	for _, tc := range testCases {
+		b.Run(strconv.Itoa(tc), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				decToBinIte(tc)
+			}
+		})
+	}
+}
+
+func BenchmarkDecToBinRec(b *testing.B) {
+	testCases := []int{0, 1, 10, 100, 1000, 10000}
+
+	for _, tc := range testCases {
+		b.Run(strconv.Itoa(tc), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				decToBinRec(tc)
+			}
+		})
+	}
+}
+
+// strconv 內建函數
+func decToBinBuiltin(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	return strconv.FormatInt(int64(n), 2)
+}
+
+func BenchmarkDecToBinBuiltin(b *testing.B) {
+	testCases := []int{0, 1, 10, 100, 1000, 10000}
+
+	for _, tc := range testCases {
+		b.Run(strconv.Itoa(tc), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				decToBinBuiltin(tc)
+			}
+		})
 	}
 }
